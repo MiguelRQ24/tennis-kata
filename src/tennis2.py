b858d30f75bf71112ad1229ae28c4d808c60f7e8
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-01-24 13:02:14 +0100

    refactor(tennis2.py): Cambiar el nombre a la variable p1points a player1_points

--- src/tennis2.py
+++ src/tennis2.py
@@ -2,8 +2,8 @@ class TennisGame2:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.p1points = 0
-        self.p2points = 0
+        self.player1_points = 0
+        self.player2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
@@ -14,15 +14,15 @@ class TennisGame2:
     def score(self):
        
         
-        if abs(self.p1points - self.p2points) > 1 and max(self.p1points, self.p2points) > 3:
-            return "Win for " + (self.player1_name if self.p1points > self.p2points else self.player2_name)
-        elif abs(self.p1points - self.p2points) == 1 and max(self.p1points, self.p2points) > 3:
-            return "Advantage " + (self.player1_name if self.p1points > self.p2points else self.player2_name)
-        elif self.p1points == self.p2points and self.p1points > 2:
+        if abs(self.player1_points - self.player2_points) > 1 and max(self.player1_points, self.player2_points) > 3:
+            return "Win for " + (self.player1_name if self.player1_points > self.player2_points else self.player2_name)
+        elif abs(self.player1_points - self.player2_points) == 1 and max(self.player1_points, self.player2_points) > 3:
+            return "Advantage " + (self.player1_name if self.player1_points > self.player2_points else self.player2_name)
+        elif self.player1_points == self.player2_points and self.player1_points > 2:
             return "Deuce"
         else:
             players_points = []
-            for player_points in [self.p1points, self.p2points]:
+            for player_points in [self.player1_points, self.player2_points]:
                 if player_points == 0:
                     players_points.append("Love")
                 elif player_points == 1:
@@ -42,7 +42,7 @@ class TennisGame2:
             self.__player2_score()
 
     def __player1_score(self):
-        self.p1points += 1
+        self.player1_points += 1
 
     def __player2_score(self):
-        self.p2points += 1
+        self.player2_points += 1
